fb79f7543dddb31a248fe1400aba7db56271c003
YC99993333 <tinoycl@gmail.com>
2020-04-01 15:30:12 +0800

    Updates

---
+++ precache-manifest.f1bc0b941932b4ba2ff97c56e2c4daa4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b756d589f328991b7ab9a751cec088cd",
+    "url": "/Pokemon/index.html"
+  },
+  {
+    "revision": "ad1412838566b7d897b8",
+    "url": "/Pokemon/static/js/2.49889234.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Pokemon/static/js/2.49889234.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73c969d14eb2c5ff50aa",
+    "url": "/Pokemon/static/js/main.d30fb590.chunk.js"
+  },
+  {
+    "revision": "25ea293a7886fd49d92a",
+    "url": "/Pokemon/static/js/runtime-main.2ed90a74.js"
+  }
+]);
